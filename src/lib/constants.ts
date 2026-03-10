d660c5c9980e2241249ae6cf72645286f3216410
Devagya Gupta <gupta.devagyav@gmail.com>
2026-03-10 13:14:12 +0000

    Preceding changes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -73,7 +73,7 @@ export const DEFAULT_UK_INPUTS: SimulationInputs = {
   currentDetectionRate: 0.33,
   oxailisDetectionRate: 0.66,
   currentFalsePositiveRate: 0.50,
-  oxailisFalsePositiveRate: 0.25,
+  oxailisFalsePositiveRate: 0.20,
   emergencyCSectionRateUndiagnosed: 0.40,
   hypoxicEventRate: 0.011,
   cerebralPalsyRisk: 0.0625,
